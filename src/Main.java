c5456df5b66435c3e2027086e6fa6d716f924fea
Vural Atici <aticivural@gmail.com>
2017-04-04 14:03:43 +0300

    add project

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //define a folder root
+        Path myDir = Paths.get("D:/data");
+
+        try {
+            WatchService watcher = myDir.getFileSystem().newWatchService();
+            myDir.register(watcher, ENTRY_CREATE,
+                                    ENTRY_DELETE,
+                                    ENTRY_MODIFY);
+
+            WatchKey watckKey = watcher.take();
+
+            List<WatchEvent<?>> events = watckKey.pollEvents();
+
+            for (WatchEvent event : events) {
+                if (event.kind() == ENTRY_CREATE) {
+                    System.out.println("Created: " + event.context().toString());
+                }
+                if (event.kind() == ENTRY_DELETE) {
+                    System.out.println("Delete: " + event.context().toString());
+                }
+                if (event.kind() == ENTRY_MODIFY) {
+                    System.out.println("Modify: " + event.context().toString());
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.toString());
+        }
+    }
+}
